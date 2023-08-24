253374567fb8777ea8363bf8b1f111ec75c2ab19
takaidohigasi <takaidohigasi@gmail.com>
2023-08-24 21:57:59 +0900

    fmt

--- internal/pimp/import.go
+++ internal/pimp/import.go
@@ -1,123 +1,123 @@
 package pimp
 
 import (
-        "context"
-        "fmt"
-        "os"
-        "os/exec"
-        "path/filepath"
-        "regexp"
-        "strings"
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"regexp"
+	"strings"
 
-        log "github.com/sirupsen/logrus"
+	log "github.com/sirupsen/logrus"
 )
 
 type ImportData struct {
-        DbName    string
-        TableName string
-        FileNum   int
-        ImportCmd string
+	DbName    string
+	TableName string
+	FileNum   int
+	ImportCmd string
 }
 
 type Plan interface {
-        Estimate() error
-        Execute() error
+	Estimate() error
+	Execute() error
 }
 
 type ImportPlan struct {
-        path        string
-        data        map[string]*ImportData
-        concurrency int
-        dbConfig    string
-        context     context.Context
+	path        string
+	data        map[string]*ImportData
+	concurrency int
+	dbConfig    string
+	context     context.Context
 }
 
 func NewImportPlan(ctx context.Context, path string, concurrency int, dbConfig string) Plan {
-        return ImportPlan{
-                context:     ctx,
-                data:        make(map[string]*ImportData),
-                path:        path,
-                concurrency: concurrency,
-                dbConfig:    dbConfig,
-        }
+	return ImportPlan{
+		context:     ctx,
+		data:        make(map[string]*ImportData),
+		path:        path,
+		concurrency: concurrency,
+		dbConfig:    dbConfig,
+	}
 }
 
 func (plan ImportPlan) Estimate() error {
-        log.Infoln("estimating import data")
-        totalFiles := 0
-        err := filepath.Walk(plan.path, func(path string, info os.FileInfo, err error) error {
-                if err != nil {
-                        return err
-                }
+	log.Infoln("estimating import data")
+	totalFiles := 0
+	err := filepath.Walk(plan.path, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
 
-                if info.IsDir() {
-                        return nil
-                }
+		if info.IsDir() {
+			return nil
+		}
 
-                if strings.HasSuffix(info.Name(), "-schema.sql") {
-                        match := regexp.MustCompile(`^(.*?)\.(.*?)-schema\.sql$`).FindStringSubmatch(info.Name())
-                        db := match[1]
-                        table := match[2]
-                        resourceId := db + "." + table
-                        plan.data[resourceId] = &ImportData{DbName: db, TableName: table}
-                        data := plan.data[resourceId]
-                        combinedColumnNames, err := ExtractColumns(path)
-                        if err != nil {
-                                log.Errorln("failed to read table definition", err, path)
-                                return err
-                        }
-                        // @see https://dev.mysql.com/doc/mysql-shell/8.0/en/mysql-shell-utilities-parallel-table.html
-                        data.ImportCmd = fmt.Sprintf("mysqlsh -- util import-table %s --schema=%s --table=%s --skipRows=1 --columns=%s --dialect=csv", plan.path + "/" + resourceId+".*.csv", db, table, combinedColumnNames)
-                }
+		if strings.HasSuffix(info.Name(), "-schema.sql") {
+			match := regexp.MustCompile(`^(.*?)\.(.*?)-schema\.sql$`).FindStringSubmatch(info.Name())
+			db := match[1]
+			table := match[2]
+			resourceId := db + "." + table
+			plan.data[resourceId] = &ImportData{DbName: db, TableName: table}
+			data := plan.data[resourceId]
+			combinedColumnNames, err := ExtractColumns(path)
+			if err != nil {
+				log.Errorln("failed to read table definition", err, path)
+				return err
+			}
+			// @see https://dev.mysql.com/doc/mysql-shell/8.0/en/mysql-shell-utilities-parallel-table.html
+			data.ImportCmd = fmt.Sprintf("mysqlsh -- util import-table %s --schema=%s --table=%s --skipRows=1 --columns=%s --dialect=csv", plan.path+"/"+resourceId+".*.csv", db, table, combinedColumnNames)
+		}
 
-                if strings.HasSuffix(info.Name(), ".csv") {
-                        match := regexp.MustCompile(`^(.*?)\.(.*?)\.(.*?)\.csv$`).FindStringSubmatch(info.Name())
-                        resourceId := match[1] + "." + match[2]
-                        data := plan.data[resourceId]
-                        data.FileNum++
-                        totalFiles++
-                }
+		if strings.HasSuffix(info.Name(), ".csv") {
+			match := regexp.MustCompile(`^(.*?)\.(.*?)\.(.*?)\.csv$`).FindStringSubmatch(info.Name())
+			resourceId := match[1] + "." + match[2]
+			data := plan.data[resourceId]
+			data.FileNum++
+			totalFiles++
+		}
 
-                return nil
-        })
-        log.Infoln("estimating import data: done")
-        log.Infoln("total files: ", totalFiles)
-        return err
+		return nil
+	})
+	log.Infoln("estimating import data: done")
+	log.Infoln("total files: ", totalFiles)
+	return err
 }
 
 func (plan ImportPlan) Execute() error {
-        log.Infoln("importing data")
-        wp := NewWorkerPool(plan.concurrency/4, plan.concurrency)
-        wp.Run()
-        for k, v := range plan.data {
-                task := func(resourceId string, data *ImportData) error {
-                        log.Infoln("load", resourceId)
-                        path := plan.path + "/" + resourceId + "-schema.sql"
-                        db := strings.Split(resourceId, ".")[0]
-                        log.Infoln(path)
-                        result, err := exec.CommandContext(plan.context, "mysql", fmt.Sprintf("--defaults-extra-file=%s", plan.dbConfig), db, "-e", fmt.Sprintf("source %s", path)).CombinedOutput()
-                        if err != nil {
-                                log.Errorln(string(result))
-                                return err
-                        }
-                        log.Infoln(data.ImportCmd)
-                        args := strings.Fields(data.ImportCmd)
-                        result, err = exec.CommandContext(plan.context, args[0], args[1:]...).CombinedOutput()
-                        if err != nil {
-                                log.Errorln(string(result))
-                                return err
-                        }
-                        log.Infoln("load %s: done", resourceId)
-                        log.Infoln(string(result))
-                        return nil
-                }
-                thread := 8
-                if v.FileNum < thread {
-                        thread = v.FileNum
-                }
-                wp.AddTask(Job{Task: task, Thread: thread, Length: v.FileNum, ResourceId: k, Data: v})
-        }
-        wp.Wait()
-        log.Infoln("importing data: done")
-        return nil
+	log.Infoln("importing data")
+	wp := NewWorkerPool(plan.concurrency/4, plan.concurrency)
+	wp.Run()
+	for k, v := range plan.data {
+		task := func(resourceId string, data *ImportData) error {
+			log.Infoln("load", resourceId)
+			path := plan.path + "/" + resourceId + "-schema.sql"
+			db := strings.Split(resourceId, ".")[0]
+			log.Infoln(path)
+			result, err := exec.CommandContext(plan.context, "mysql", fmt.Sprintf("--defaults-extra-file=%s", plan.dbConfig), db, "-e", fmt.Sprintf("source %s", path)).CombinedOutput()
+			if err != nil {
+				log.Errorln(string(result))
+				return err
+			}
+			log.Infoln(data.ImportCmd)
+			args := strings.Fields(data.ImportCmd)
+			result, err = exec.CommandContext(plan.context, args[0], args[1:]...).CombinedOutput()
+			if err != nil {
+				log.Errorln(string(result))
+				return err
+			}
+			log.Infoln("load %s: done", resourceId)
+			log.Infoln(string(result))
+			return nil
+		}
+		thread := 8
+		if v.FileNum < thread {
+			thread = v.FileNum
+		}
+		wp.AddTask(Job{Task: task, Thread: thread, Length: v.FileNum, ResourceId: k, Data: v})
+	}
+	wp.Wait()
+	log.Infoln("importing data: done")
+	return nil
 }
--- internal/pimp/worker.go
+++ internal/pimp/worker.go
@@ -1,90 +1,90 @@
 package pimp
 
 import (
+	log "github.com/sirupsen/logrus"
 	"sync"
-        log "github.com/sirupsen/logrus"
 )
 
 type Job struct {
-        Task   func(string, *ImportData) error
-        Thread int
-        Length int
-        ResourceId      string
-        Data            *ImportData
+	Task       func(string, *ImportData) error
+	Thread     int
+	Length     int
+	ResourceId string
+	Data       *ImportData
 }
 
 type WorkerPool interface {
-        Run()
-        AddTask(Job)
-        Wait()
+	Run()
+	AddTask(Job)
+	Wait()
 }
 
 type workerPool struct {
-        maxWorker   int
-        maxCPU      int
-        queuedTaskC chan Job
-        progress    Progress
-        wg          sync.WaitGroup
+	maxWorker   int
+	maxCPU      int
+	queuedTaskC chan Job
+	progress    Progress
+	wg          sync.WaitGroup
 }
 
 type Progress struct {
-        mutex       sync.Mutex
-        concurrency int
-        completed   int
+	mutex       sync.Mutex
+	concurrency int
+	completed   int
 }
 
 func (p *Progress) start(thread int) {
-        p.mutex.Lock()
-        defer p.mutex.Unlock()
-        p.concurrency += thread
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	p.concurrency += thread
 }
 
 func (p *Progress) finish(thread int, file int) {
-        p.mutex.Lock()
-        defer p.mutex.Unlock()
-        p.concurrency -= thread
-        p.completed += file
+	p.mutex.Lock()
+	defer p.mutex.Unlock()
+	p.concurrency -= thread
+	p.completed += file
 }
 
 func (wp *workerPool) Run() {
-        wp.run()
+	wp.run()
 }
 
 func (wp *workerPool) AddTask(task Job) {
-        wp.wg.Add(1)
-        wp.queuedTaskC <- task
+	wp.wg.Add(1)
+	wp.queuedTaskC <- task
 }
 
 func (wp *workerPool) Wait() {
-        wp.wg.Wait()
+	wp.wg.Wait()
 }
 
 func (wp *workerPool) run() {
-        wp.progress.concurrency = 0
-        for i := 0; i < wp.maxWorker; i++ {
-                wID := i + 1
-                go func(workerID int) {
-                        for task := range wp.queuedTaskC {
-                                if wp.maxCPU-wp.progress.concurrency >= task.Thread {
-                                        wp.progress.start(task.Thread)
-                                        if err := task.Task(task.ResourceId, task.Data); err != nil {
-                                                log.Infoln("error", err)
-                                        }
-                                        wp.progress.finish(task.Thread, task.Length)
-                                        wp.wg.Done()
-                                }
-                        }
-                }(wID)
-        }
+	wp.progress.concurrency = 0
+	for i := 0; i < wp.maxWorker; i++ {
+		wID := i + 1
+		go func(workerID int) {
+			for task := range wp.queuedTaskC {
+				if wp.maxCPU-wp.progress.concurrency >= task.Thread {
+					wp.progress.start(task.Thread)
+					if err := task.Task(task.ResourceId, task.Data); err != nil {
+						log.Infoln("error", err)
+					}
+					wp.progress.finish(task.Thread, task.Length)
+					wp.wg.Done()
+				}
+			}
+		}(wID)
+	}
 }
 
 func NewWorkerPool(maxWorker int, maxCPU int) WorkerPool {
-        wp := &workerPool{
-                maxWorker:   maxWorker,
-                maxCPU:      maxCPU,
-                queuedTaskC: make(chan Job),
-                progress:    Progress{},
-                wg:          sync.WaitGroup{},
-        }
-        return wp
+	wp := &workerPool{
+		maxWorker:   maxWorker,
+		maxCPU:      maxCPU,
+		queuedTaskC: make(chan Job),
+		progress:    Progress{},
+		wg:          sync.WaitGroup{},
+	}
+	return wp
 }
